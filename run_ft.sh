5ed61dddfa04d408afc5e35521cf7acec6ae3dc1
Ceng Zihao <57060623+CengZihao@users.noreply.github.com>
2024-01-16 09:14:00 +0800

    Update run_ft.sh

--- run_ft.sh
+++ run_ft.sh
@@ -2,7 +2,6 @@
 set -x
 
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-# export LD_LIBRARY_PATH=/data/zzh/d2l/miniconda3/envs/O-LoRA/lib:$LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES="1,2,3,4,5,6"
 
 # bash run_ft.sh
@@ -15,14 +14,14 @@ echo -e "==================================================\n" \
     "==================================================\n\n" \
     > log/$version.log
 
+# /data/zzh/d2l/miniconda3/envs/lorao/bin/python /data/zzh/olora/finetune.py \
+#     --output_dir "./lora-alpaca/$version" \
+#     >> log/$version.log 2>&1
+
 /data/zzh/d2l/miniconda3/envs/lorao/bin/python /data/zzh/olora/finetune.py \
     --output_dir "./lora-alpaca/$version" \
     --resume_from_checkpoint "/data/zzh/olora/lora-alpaca/0115_v1_piqa_tau-2" \
     >> log/$version.log 2>&1
-
-# /data/zzh/d2l/miniconda3/envs/lorao/bin/python /data/zzh/olora/finetune.py \
-#     --output_dir "./lora-alpaca/$version" \
-#     >> log/$version.log 2>&1
 
 # /data/zzh/d2l/miniconda3/envs/lora/bin/python /data/zzh/olora/finetune_traditional.py \
 #     --output_dir "./lora-alpaca/$version" \
